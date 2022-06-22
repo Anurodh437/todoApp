64df00b375ce68a85a051e7ddbe24772ecb48dfa
Gem-Anurodh <anurodh.dubey@geminisolutions.com>
2022-06-22 23:01:07 +0530

    added the crud functionality

--- src/app/main/todo/todo.component.ts
+++ src/app/main/todo/todo.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudTaskService } from 'src/app/crud-task.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  
+  public task: string;
+  constructor(private crudService: CrudTaskService) {
+    this.task = '';
   }
 
+  ngOnInit(): void {}
+  
+
+  public addTask(): void {
+    this.crudService.addTask(this.task);
+    this.task = '';
+  }
 }
