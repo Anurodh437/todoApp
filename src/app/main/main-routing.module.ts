721f7fa976bf44f072b2fd5449c2ba3e6d750f33
pushpender2 <pushpender.singh@geminisolutions.com>
2022-08-05 22:03:04 +0530

    added json server and created login singup page with routing

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 import { TodoComponent } from './todo/todo.component';
 import { ViewTaskComponent } from './view-task/view-task.component';
 
 const routes: Routes = [
-  { path: '', component: TodoComponent },
+  { path: 'addTask', component: TodoComponent },
   { path: 'tasks', component: ViewTaskComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: SignupComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class MainRoutingModule {}
+export class MainRoutingModule { }
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,11 +4,17 @@ import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { MainRoutingModule } from './main-routing.module';
 import { TodoComponent } from './todo/todo.component';
 import { ViewTaskComponent } from './view-task/view-task.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     TodoComponent,
     ViewTaskComponent,
+    LoginComponent,
+    SignupComponent,
+    HttpClientModule
   ],
   imports: [
     CommonModule,
