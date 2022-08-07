36476b2826946724bde7026ae45abba4ac0323d3
pushpender2 <pushpender.singh@geminisolutions.com>
2022-08-07 22:33:12 +0530

    created login functionality

--- src/app/main/login/login.component.ts
+++ src/app/main/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from "ngx-spinner";
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/service/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,32 +12,28 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private toastr: ToastrService, private spinner: NgxSpinnerService, private _fb: FormBuilder) { }
+  constructor(private toastr: ToastrService, private spinner: NgxSpinnerService, private _fb: FormBuilder, public api: ApiService, private router: Router) { }
   public loginForm!: FormGroup;
   ngOnInit(): void {
     this.loginForm = this._fb.group({
-      email: ["", [Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
-      password: ["", [Validators.required,Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")]],
-      check:[false]
+      email: ["", [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      password: ["", [Validators.required, Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")]],
+      check: [false]
     })
   }
 
-  get email(){
+  get email() {
     return this.loginForm.get('email');
   }
 
-  get password(){
+  get password() {
     return this.loginForm.get('password');
   }
 
   login() {
-    if(this.loginForm.invalid){
-    this.toastr.error('Required Field cannot be Empty!');
+    if (this.loginForm.invalid) {
+      this.toastr.error('Required Field cannot be Empty!');
     }
-    // this.spinner.show();
-    // setTimeout(() => {
-    //   this.spinner.hide();
-    // }, 1000);
-    console.log(this.loginForm.get('email'));
+    this.api.getUsers(this.loginForm);
   }
 }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,17 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private toastr: ToastrService, private spinner: NgxSpinnerService, private router: Router) { }
 
-  getUsers() {
-    return this.http.get("http://localhost:3000/registeredUser")
+  getUsers(loginForm: any) {
+    return this.http.get<any>("http://localhost:3000/registeredUser")
       .subscribe(res => {
-        return res;
+        const user = res.find((val: any) => {
+          return val.email === loginForm.value.email && val.password === loginForm.value.password;
+        });
+        if (user) {
+          this.toastr.success("logged in", "Success", { timeOut: 1000 });
+          this.spinner.show();
+          setTimeout(() => {
+            this.spinner.hide();
+          }, 2000);
+          this.router.navigate(['/addTask']);
+          localStorage.setItem('userDetails', JSON.stringify(loginForm.value));
+        }
+        else {
+          this.toastr.error("Invalid Credentials", "Error", { timeOut: 1000 })
+        }
       });
   }
 
